b778ee7ff918f762c35455d7385e267e50c2f0f6
sergiogomes <sergiopgomes92@gmail.com>
2020-10-21 06:05:11 -0300

    Completing Landing Page.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,17 +1,35 @@
 import React from "react"
 import { View, Text, StyleSheet, Dimensions, Image } from "react-native";
 
+const screenWidth = Dimensions.get('screen').width;
+
 export const LandingPage = () => {
+  const {
+    container,
+    navigation,
+    body,
+    deliveryIcon,
+    addressContainer,
+    addressTitle,
+    addressText,
+    footer
+  } = styles;
+
   return (
-    <View style={styles.container}>
-      <View style={styles.navigation}>
-        <Text> Navigation </Text>
+    <View style={container}>
+      <View style={navigation}>
       </View>
-      <View style={styles.body}>
-        <Text> Landing Page </Text>
+      <View style={body}>
+        <Image
+          source={require('../images/delivery_icon.png')}
+          style={deliveryIcon}  
+        />
+        <View style={addressContainer}>
+          <Text style={addressTitle}> Your Delivery Address </Text>
+        </View>
+        <Text style={addressText}> Waiting for Current Location </Text>
       </View>
-      <View style={styles.footer}>
-        <Text> Footer </Text>
+      <View style={footer}>
       </View>
     </View>
   );
@@ -19,21 +37,40 @@ export const LandingPage = () => {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'rgb(242, 242, 242)',
     flex: 1,
-    backgroundColor: 'green'
   },
   navigation: {
     flex: 2,
-    backgroundColor: 'red'
   },
   body: {
+    alignItems: 'center',
     flex: 9,
     justifyContent: 'center',
+  },
+  deliveryIcon: {
+    height: 120,
+    width: 120,
+  },
+  addressContainer: {
     alignItems: 'center',
-    backgroundColor: 'yellow'
+    borderBottomColor: 'red',
+    borderBottomWidth: 0.5,
+    marginBottom: 10,
+    padding: 5,
+    width: screenWidth - 100,
+  },
+  addressTitle: {
+    color: '#7D7D7D',
+    fontSize: 22,
+    fontWeight: '700',
+  },
+  addressText: {
+    color: '#4F4F4F',
+    fontSize: 20,
+    fontWeight: '200',
   },
   footer: {
     flex: 1,
-    backgroundColor: 'cyan'
   }
 })
